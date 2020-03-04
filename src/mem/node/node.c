393c02edceda90f831332ae1a8d8de145e699e5f
r0ck3r008 <naman.arora@tutanota.com>
2020-03-04 18:05:54 -0500

    1. Added child pointer in struct node and initialized to NULL 2. init_node function is now init_list 3. Added move_first function for a list 4. de_init_list now first calls move_first before deleting 5. List is a doubliy linked list, not doubly singular 6. Added a new function, remove_node as a pre setup step for delete node

--- src/mem/node/node.c
+++ src/mem/node/node.c
@@ -2,34 +2,39 @@
 
 #include"node.h"
 
-struct node *init_node(void *payload, int val)
+struct node *init_list(void *payload, int val)
 {
 	struct node *new_node=alloc("struct node", 1);
 	new_node->val=val;
 	new_node->payload=payload;
 	new_node->parent=NULL;
+	new_node->child=NULL;
 	new_node->nxt=NULL;
 	new_node->prev=NULL;
 
 	return new_node;
 }
 
-struct node *init_list()
-{
-	struct node *start=init_node("START", -1);
-	return start;
-}
-
 void de_init_list(struct node *start)
 {
 	int ret=1;
 	struct node *curr;
+	start=move_first(start);
 	for(curr; curr=start; curr->nxt!=NULL) {
 		del_node(curr->nxt);
 		curr=start->nxt;
 	}
 
 	dealloc(start, "struct node", 1);
+}
+
+struct node *move_first(struct node *start)
+{
+	struct node *curr;
+	for(curr; curr=start->prev; curr->prev!=NULL)
+		curr=curr->prev;
+
+	return curr;
 }
 
 void add_node(struct node *start, struct node *new_node)
@@ -40,12 +45,17 @@ void add_node(struct node *start, struct node *new_node)
 
 	curr->nxt=new_node;
 	new_node->prev=curr;
+	new_node->nxt=NULL;
+}
+
+void remove_node(struct node *trgt)
+{
+	trgt->prev->nxt=trgt->nxt;
+	trgt->nxt->prev=trgt->prev;
 }
 
 void del_node(struct node *trgt)
 {
-	trgt->prev->nxt=trgt->nxt;
-	if(trgt->nxt!=NULL)
-		trgt->nxt->prev=trgt->prev;
+	remove_node(trgt);
 	dealloc(trgt, "struct node", 1);
 }
--- src/mem/node/node.h
+++ src/mem/node/node.h
@@ -6,15 +6,16 @@
 struct node
 {
 	int val;
-	struct node *parent;
+	struct node *parent, *child;
 	struct node *nxt, *prev;
 	void *payload;
 };
 
-struct node *init_node(void *, int);
-struct node *init_list();
+struct node *init_list(void *, int);
 void de_init_list(struct node *);
+struct node *move_first(struct node *);
 void add_node(struct node *, struct node *);
+void remove_node(struct node *);
 void del_node(struct node *);
 
 #endif
