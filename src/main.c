6b4be786f34a3204fec7d483d29cbc885b9eea81
r0ck3r008 <naman.arora@tutanota.com>
2020-03-05 17:47:09 -0500

    added driver code

---
+++ src/main.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sodium.h>
+
+#include"mem/mem.h"
+
+int main(int argc, char *argv[])
+{
+	if(sodium_init()<0) {
+		fprintf(stderr, "Error in initiating the sodiumlib!\n");
+		_exit(-1);
+	}
+
+	uint32_t upper_bound=1000;
+	struct heap *test_heap;
+	for(int i=0; i<5; i++) {
+		uint32_t rand_num=randombytes_uniform(upper_bound);
+		printf("Rand num: %d\n", rand_num);
+		if(!i) {
+			test_heap=init_heap(NULL, rand_num);
+		} else {
+
+			insert(test_heap, NULL, rand_num);
+		}
+	}
+
+	print_heap(test_heap);
+
+	/*
+	for(int i=0; i<5; i++) {
+		struct node *max=remove_max(test_heap);
+		printf("Key: %d\n", max->val);
+	}
+	*/
+
+	de_init_heap(test_heap);
+}
