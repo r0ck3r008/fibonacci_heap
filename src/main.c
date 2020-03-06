08bd0f020a5e8b4ad1dd96ef5092742c07d68d0b
r0ck3r008 <naman.arora@tutanota.com>
2020-03-06 01:57:23 -0500

    added test for print

--- src/main.c
+++ src/main.c
@@ -15,7 +15,6 @@ int main(int argc, char *argv[])
 	struct heap *test_heap;
 	for(int i=0; i<5; i++) {
 		uint32_t rand_num=randombytes_uniform(upper_bound);
-		printf("Rand num: %d\n", rand_num);
 		if(!i) {
 			test_heap=init_heap(NULL, rand_num);
 		} else {
@@ -24,9 +23,12 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	print_heap(test_heap);
+
 	for(int i=0; i<5; i++) {
 		struct node *max=remove_max(test_heap);
 		printf("Key: %d\n", max->val);
+		print_heap(test_heap);
 		dealloc_node(max);
 	}
 
