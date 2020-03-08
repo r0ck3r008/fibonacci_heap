4a7f93313b9f6dc126e4782365f709f2df21629d
r0ck3r008 <naman.arora@tutanota.com>
2020-03-07 21:04:55 -0500

    1. Added struct node ** in alloc/dealloc 2. Added struct hmap * in alloc/dealloc 3. Added struct data * in alloc/dealloc

--- src/mem/alloc/alloc.c
+++ src/mem/alloc/alloc.c
@@ -12,14 +12,21 @@ void *alloc(char *type, int size)
 	if(!strcmp(type, "char")) {
 		ret=malloc(size*sizeof(char));
 		explicit_bzero(ret, size*sizeof(char));
-
 	} else if(!strcmp(type, "struct node")) {
 		ret=malloc(size*sizeof(struct node));
 		explicit_bzero(ret, size*sizeof(struct node));
-
+	} else if(!strcmp(type, "struct node *")) {
+		ret=malloc(size*sizeof(struct node *));
+		explicit_bzero(ret, size*sizeof(struct node *));
 	} else if(!strcmp(type, "struct heap")) {
 		ret=malloc(size*sizeof(struct heap));
 		explicit_bzero(ret, sizeof(struct heap)*size);
+	} else if(!strcmp(type, "struct data")) {
+		ret=malloc(size*sizeof(struct data));
+		explicit_bzero(ret, size*sizeof(struct data));
+	} else if(!strcmp(type, "struct hmap")) {
+		ret=malloc(size*sizeof(struct hmap));
+		explicit_bzero(ret, sizeof(struct hmap)*size);
 	}
 
 	if(ret==NULL) {
@@ -37,8 +44,14 @@ void dealloc(void *buf, char *type, int size)
 		explicit_bzero((char *)buf, sizeof(char)*size);
 	} else if(!strcmp(type, "struct node")) {
 		explicit_bzero((struct node *)buf, sizeof(struct node)*size);
+	} else if(!strcmp(type, "struct node *")) {
+		explicit_bzero((struct node **)buf, size*sizeof(struct node *));
 	} else if(!strcmp(type, "struct heap")) {
 		explicit_bzero((struct heap *)buf, sizeof(struct heap)*size);
+	} else if(!strcmp(type, "struct data")) {
+		explicit_bzero((struct data *)buf, size*sizeof(struct data));
+	} else if(!strcmp(type, "struct hmap")) {
+		explicit_bzero((struct hmap *)buf, sizeof(struct hmap)*size);
 	}
 
 	free(buf);
