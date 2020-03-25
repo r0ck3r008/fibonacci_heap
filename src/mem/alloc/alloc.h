140ae83f6ff2b525ff392738665a637f3987a197
r0ck3r008 <naman.arora@tutanota.com>
2020-03-25 00:50:06 -0400

    branch cleanup

--- src/mem/alloc/alloc.h
+++
@@ -1,9 +0,0 @@
-#ifndef ALLOC_H
-#define ALLOC_H
-
-#include"../mem.h"
-
-void *alloc(char *, int);
-void dealloc(void *, char *, int);
-
-#endif
--- src/mem/mem.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEM_H
-#define MEM_H
-
-#include"alloc/alloc.h"
-#include"node/node.h"
-#include"heap/heap.h"
-#include"hmap/hmap.h"
-
-#endif
